4f64b03e459e832c409cec8d75eb52c7f560df7b
nalee97 <mohamednaleef1564@gmail.com>
2022-10-21 13:41:48 +0530

    Add project files.

---
+++ BlazorWebApp/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public static List<Comic> comics = new List<Comic> 
+        {
+            new Comic {Id = 1, Name = "Marvel"},
+            new Comic {Id = 2, Name = "DC"}
+        };
+
+        public static List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero {
+                Id = 1,
+                FirstName = "Robert",
+                LastName = "Downey Jr",
+                HeroName = "Iron Man",
+                Comic = comics[0]
+            },
+
+           new SuperHero {
+                Id = 2,
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                HeroName = "Bat Man",
+                Comic = comics[1]
+            },
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var hero = heroes.FirstOrDefault(h => h.Id == id);
+            if (hero == null)
+            {
+                return NotFound("No hero found");
+            }
+            return Ok(hero);
+        }
+    }
+}
